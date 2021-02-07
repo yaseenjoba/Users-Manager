f208ba9dc2da51e396f4c7c05f6a1005b2314d80
yaseenjoba <46541853+yaseenjoba@users.noreply.github.com>
2021-02-07 18:07:08 +0200

    add style

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,9 @@ import { UsersComponent } from './users/users.component';
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [{path:"edit",component:EditUsersComponent},{path:"home",component:HomeComponent},{path:"",component:HomeComponent},{path:"users",component:UsersComponent},{path:"add",component:AddUsersComponent}];
+const routes: Routes = [{path:"about",component:AboutComponent},{path:"edit",component:EditUsersComponent},{path:"home",component:HomeComponent},{path:"",component:HomeComponent},{path:"users",component:UsersComponent},{path:"add",component:AddUsersComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
