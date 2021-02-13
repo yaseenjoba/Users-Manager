56a66c57443052b53da52135ec38c1dca6aa19b9
yaseenjoba <46541853+yaseenjoba@users.noreply.github.com>
2021-02-13 22:30:34 +0200

    5th-3 edit for deploy

--- server.js
+++ server.js
@@ -14,3 +14,6 @@ app.get('/*', function(req, res) {
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+var firebase = require("firebase/app");
+require("firebase/storage");
+const storageRef = firebase.storage().ref();
